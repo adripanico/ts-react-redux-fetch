5567231c08f350db8c93661041b16ece306f49a7
Adrian Caballero <adripanicodev@gmail.com>
2023-08-08 17:06:48 +0200

    chore: bump dependencies and adding eslint plugins & rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,70 @@
+const ALIASES = '^(components|http|models|store)/';
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['import', 'prettier', 'react-refresh', 'simple-import-sort'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    'prettier/prettier': 'error',
+    curly: ['error', 'all'],
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Packages.
+          // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+          ['^@?\\w'],
+          // Absolute imports and other imports such as Vue-style `@/foo`.
+          // Anything not matched in another group.
+          ['^'],
+          // alias paths
+          //? also change in the rule import/no-unresolved
+          [ALIASES],
+          // Relative imports.
+          // Anything that starts with a dot.
+          ['^\\.'],
+          // scss module imports
+          ['\\.module\\.scss$'],
+          // Side effect imports.
+          ['^\\u0000'],
+        ],
+      },
     ],
+    'simple-import-sort/exports': 'error',
+    'import/no-anonymous-default-export': 'off',
+    'import/first': 'error',
+    //? also change in simple-import-sort/imports to group them
+    'import/no-unresolved': [
+      'error',
+      {
+        ignore: ['.(scss|less|css)$', ALIASES],
+      },
+    ],
+    'import/newline-after-import': 'off',
+    'import/no-duplicates': 'error',
+    // try to add eslint-ignore in important console.logs
+    // set to warn to allow webpack compile it
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-non-null-assertion': 'off',
   },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/ts-react-redux-fetch/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, './src/components'),
+      http: path.resolve(__dirname, './src/http'),
+      models: path.resolve(__dirname, './src/models'),
+      store: path.resolve(__dirname, './src/store'),
+    },
+  },
 });
