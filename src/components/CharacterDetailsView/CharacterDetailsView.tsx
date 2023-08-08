f200950010f55458715ce019d9e6c025a5f41544
Adrian Caballero <adripanicodev@gmail.com>
2023-08-08 19:04:41 +0200

    feat: adding link to navigate back from character details

--- src/components/CharacterDetailsView/CharacterDetailsView.tsx
+++ src/components/CharacterDetailsView/CharacterDetailsView.tsx
@@ -1,12 +1,14 @@
-import { Navigate, useParams } from 'react-router-dom';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
 
+import { ArrowBackIcon } from 'shared/icons/ArrowBackIcon';
 import { selectCharacter } from 'store/characters/characters.selector';
 import { useAppSelector } from 'store/store';
 
-// import styles from './CharacterDetailsView.module.scss';
+import styles from './CharacterDetailsView.module.scss';
 
 export const CharacterDetailsView = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
 
   const character = useAppSelector((state) => selectCharacter(state, id));
 
@@ -14,5 +16,16 @@ export const CharacterDetailsView = () => {
     return <Navigate to="/characters" replace={true} />;
   }
 
-  return <pre>{JSON.stringify(character, null, 2)}</pre>;
+  return (
+    <div className={styles.characterDetails}>
+      <h1 className={styles.goBack} onClick={() => navigate('/characters')}>
+        <ArrowBackIcon /> Back to characters
+      </h1>
+
+      {/* TO BE CONTINUED */}
+      <div className={styles.info}>
+        <pre>{JSON.stringify(character, null, 2)}</pre>
+      </div>
+    </div>
+  );
 };
