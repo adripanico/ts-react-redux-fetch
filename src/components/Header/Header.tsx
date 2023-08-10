11e83f678ceff255570bf471da46e4ff8ca83b82
Adrian Caballero <adripanicodev@gmail.com>
2023-08-10 19:44:17 +0200

    fix: fixing broken images

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
 import { ThemeSwitcher } from 'shared/ThemeSwitcher/ThemeSwitcher';
 
+import logo from './logo.png';
+
 import styles from './Header.module.scss';
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <img src="/logo.png" height={56} />
+      <img src={logo} height={56} alt="Rick and Morty logo" />
       <ThemeSwitcher />
     </header>
   );
